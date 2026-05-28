5922faf596e83798695123f2e7c6d40f27face5c
elliot <elliot@wyrick.ws>
2026-05-27 23:07:02 -0400

    add OpenCV integration

---
+++ src/main.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main() {
+	std::cout << "Hello world" << std::endl;
+	
+	cv::VideoCapture cap(0);
+	if(!cap.isOpened()){
+		std::cerr << "failed" << std::endl;
+		return -1;
+	}
+
+	cv::Mat frame;
+
+	cap >> frame;
+
+	if(frame.empty()) return -1;
+	
+	cv::imwrite("/mnt/myshare/img/image.png", frame);
+	std::cout << "open cv version: " << CV_VERSION <<std::endl;	
+		
+	return 0;
+
+}
